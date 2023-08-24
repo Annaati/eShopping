6d48f1fdc9ca381e9bf12ed5caefd5b1ba20ba96
Annaati <Abdi17fatah@gmail.com>
2023-08-24 17:24:53 +0300

    Catalog.Infrastructure: Added/Implemented Type Seed json Data from file

---
+++ Services/Catalog/Catalog.Infrastructure/Data/TypeContextSeed.cs
@@ -0,0 +1,36 @@
+﻿using Catalog.Core.Entities;
+using MongoDB.Driver;
+using Newtonsoft.Json;
+
+namespace Catalog.Infrastructure.Data
+{
+    public static class TypeContextSeed
+    {
+
+        public static void SeedData(IMongoCollection<ProductType> typeCollection)
+        {
+            bool checkTypes = typeCollection.Find(p => true).Any();
+
+            string path = Path.Combine("Data", "SeedData", "types.json");
+
+            if (!checkTypes)
+            {
+                string brandsData = File.ReadAllText(path);
+
+                var typesList = JsonConvert.DeserializeObject<List<ProductType>>(brandsData);
+
+                if(typesList != null)
+                {
+                    foreach (var type in typesList)
+                    {
+                        typeCollection.InsertOneAsync(type);
+                    }
+                }
+            }
+
+
+            
+        }
+    
+    }
+}
