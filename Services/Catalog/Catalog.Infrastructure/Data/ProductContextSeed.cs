ae38a4955d9d80ef2ce7d493dfc58733e2f73bfa
Annaati <Abdi17fatah@gmail.com>
2023-08-24 17:24:12 +0300

    Catalog.Infrastructure: Added/Implemented Product Seed json Data from file

---
+++ Services/Catalog/Catalog.Infrastructure/Data/ProductContextSeed.cs
@@ -0,0 +1,36 @@
+﻿using Catalog.Core.Entities;
+using MongoDB.Driver;
+using Newtonsoft.Json;
+
+namespace Catalog.Infrastructure.Data
+{
+    public static class ProductContextSeed
+    {
+
+        public static void SeedData(IMongoCollection<Product> productCollection)
+        {
+            bool checkProducts = productCollection.Find(p => true).Any();
+
+            string path = Path.Combine("Data", "SeedData", "products.json");
+
+            if (!checkProducts)
+            {
+                string productsData = File.ReadAllText(path);
+
+                var productsList = JsonConvert.DeserializeObject<List<Product>>(productsData);
+
+                if(productsList != null)
+                {
+                    foreach (var product in productsList)
+                    {
+                        productCollection.InsertOneAsync(product);
+                    }
+                }
+            }
+
+
+            
+        }
+    
+    }
+}
