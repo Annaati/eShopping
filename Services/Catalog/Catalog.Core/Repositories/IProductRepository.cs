77b877f4f0cc71d107382bd48ccc72f0bfa5d4e9
Annaati <Abdi17fatah@gmail.com>
2023-08-24 13:01:39 +0300

    Catalog.Core: Added IRepositories - Product, Type, Brand

---
+++ Services/Catalog/Catalog.Core/Repositories/IProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using Catalog.Core.Entities;
+
+namespace Catalog.Core.Repositories
+{
+    public interface IProductRepository
+    {
+
+        Task<IEnumerable<Product>> GetAllProducts();
+        Task<Product> GetProductById(string id);
+        Task<IEnumerable<Product>> GetProductByName(string name);
+        Task<IEnumerable<Product>> GetProductByBrand(string brand);
+        Task<IEnumerable<Product>> GetProductByType(string type);
+        Task CreateProduct(Product product);
+        Task<bool> UpdateProduct(Product product);
+        Task<bool> DeleteProduct(string id);
+    }
+}
