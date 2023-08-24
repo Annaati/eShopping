fbe7b96c9a8544a46781417d15057dd600a3c5f1
Annaati <Abdi17fatah@gmail.com>
2023-08-24 17:24:29 +0300

    Catalog.Infrastructure: Added/Implemented Brand Seed json Data from file

---
+++ Services/Catalog/Catalog.Infrastructure/Data/BrandContextSeed.cs
@@ -0,0 +1,36 @@
+﻿using Catalog.Core.Entities;
+using MongoDB.Driver;
+using Newtonsoft.Json;
+
+namespace Catalog.Infrastructure.Data
+{
+    public static class BrandContextSeed
+    {
+
+        public static void SeedData(IMongoCollection<ProductBrand> brandCollection)
+        {
+            bool checkBrands = brandCollection.Find(p => true).Any();
+
+            string path = Path.Combine("Data", "SeedData", "brands.json");
+
+            if (!checkBrands)
+            {
+                string brandsData = File.ReadAllText(path);
+
+                var brandsList = JsonConvert.DeserializeObject<List<ProductBrand>>(brandsData);
+
+                if(brandsList != null)
+                {
+                    foreach (var brand in brandsList)
+                    {
+                        brandCollection.InsertOneAsync(brand);
+                    }
+                }
+            }
+
+
+            
+        }
+    
+    }
+}
