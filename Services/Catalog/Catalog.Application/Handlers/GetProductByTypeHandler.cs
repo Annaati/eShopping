59f8ca2a75a700e7e2cf0227817e5fbef6e1fd84
Annaati <Abdi17fatah@gmail.com>
2023-08-26 04:38:06 +0300

    Catalog.Application: Added/Implemented Get Product By Type Query and Handler

---
+++ Services/Catalog/Catalog.Application/Handlers/GetProductByTypeHandler.cs
@@ -0,0 +1,25 @@
+﻿using Catalog.Application.Mappers;
+using Catalog.Application.Queries;
+using Catalog.Core.Application.Responses;
+using Catalog.Core.Repositories;
+using MediatR;
+
+namespace Catalog.Application.Handlers
+{
+    public class GetProductByTypeHandler : IRequestHandler<GetProductByTypeQuery, IList<ProductResponse>>
+    {
+        private readonly IProductRepository _productRepository;
+        public GetProductByTypeHandler(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public async Task<IList<ProductResponse>> Handle(GetProductByTypeQuery request, CancellationToken cancellationToken)
+        {
+            var productsList = await _productRepository.GetProductByType(request.Type);
+            var productsResponse = ProductMapper.mapper.Map<IList<ProductResponse>>(productsList);
+
+            return productsResponse;
+        }
+    }
+}
---
+++ Services/Catalog/Catalog.Application/Queries/GetProductByTypeQuery.cs
@@ -0,0 +1,15 @@
+﻿using Catalog.Core.Application.Responses;
+using MediatR;
+
+namespace Catalog.Application.Queries
+{
+    public class GetProductByTypeQuery : IRequest<IList<ProductResponse>>
+    {
+        public string Type { get; set; }
+
+        public GetProductByTypeQuery(string type)
+        {
+            Type = type;
+        }
+    }
+}
