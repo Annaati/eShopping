a66b502fea79df0e0a9e2fac45f22c55beb19e98
Annaati <Abdi17fatah@gmail.com>
2023-08-26 03:42:02 +0300

    Catalog.Application: Added/Implemented Product Response

---
+++ Services/Catalog/Catalog.Application/Responses/ProductResponse.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using Catalog.Core.Entities;
+
+namespace Catalog.Core.Application.Responses
+{
+    public class ProductResponse
+    {
+        public string Id { get; set; }
+        [BsonElement("Name")]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Summery { get; set; }
+        public string ImageFile { get; set; }
+        public ProductBrand Brands { get; set; }
+        public ProductType Types { get; set; }
+        [BsonRepresentation(BsonType.Decimal128)]
+        public decimal Price { get; set; }
+    }
+}
