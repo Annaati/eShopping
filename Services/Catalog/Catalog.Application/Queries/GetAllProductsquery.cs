3cf1467ac926d1b17c1a4f9ef4fc2ab64b8568dd
Annaati <Abdi17fatah@gmail.com>
2023-08-26 03:42:30 +0300

    Catalog.Application: Added/Implemented Get All Products Query

---
+++ Services/Catalog/Catalog.Application/Queries/GetAllProductsquery.cs
@@ -0,0 +1,9 @@
+﻿using Catalog.Core.Application.Responses;
+using MediatR;
+
+namespace Catalog.Application.Queries
+{
+    public class GetAllProductsquery : IRequest<IList<ProductResponse>>
+    {
+    }
+}
