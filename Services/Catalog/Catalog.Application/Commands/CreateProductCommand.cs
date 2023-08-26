55068db0ba042df168cea75bd74efdd93a2875b3
Annaati <Abdi17fatah@gmail.com>
2023-08-26 22:19:12 +0300

    Catalog.Application: Added/Implemenetd Create/Delete Product Query(delete), Command(create) and Handlers

---
+++ Services/Catalog/Catalog.Application/Commands/CreateProductCommand.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using Catalog.Core.Application.Responses;
+using Catalog.Core.Entities;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace Catalog.Application.Commands
+{
+    public class CreateProductCommand : IRequest<ProductResponse>
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Summery { get; set; }
+        public string ImageFile { get; set; }
+        public ProductBrand Brands { get; set; }
+        public ProductType Types { get; set; }
+        [BsonRepresentation(BsonType.Decimal128)]
+        public decimal Price { get; set; }
+    }
+}
---
+++ Services/Catalog/Catalog.Application/Handlers/CreateProductHandler.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using Catalog.Application.Commands;
+using Catalog.Core.Application.Responses;
+using Catalog.Core.Repositories;
+using Catalog.Application.Mappers;
+using Catalog.Core.Entities;
+
+namespace Catalog.Application.Handlers
+{
+    public class CreateProductHandler : IRequestHandler<CreateProductCommand, ProductResponse>
+    {
+        private readonly IProductRepository _productRepository;
+        public CreateProductHandler(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+        public async Task<ProductResponse> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            var ProductEntity = ProductMapper.mapper.Map<Product>(request);
+
+            if(ProductEntity == null)
+            {
+                throw new ArgumentException(nameof(request));
+            }
+
+            var newProuduct = await _productRepository.CreateProduct(ProductEntity);
+            var ProductResponse = ProductMapper.mapper.Map<ProductResponse>(newProuduct);
+
+            return ProductResponse;
+        }
+    }
+}
