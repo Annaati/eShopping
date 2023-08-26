9c91a37ef2f74496ea78639b9cf499f7f353eec8
Annaati <Abdi17fatah@gmail.com>
2023-08-26 04:36:08 +0300

    Catalog.Application: Added/Implemented Get Product By Id Query and Handler

---
+++ Services/Catalog/Catalog.Application/Handlers/GetProductByIdHandler.cs
@@ -0,0 +1,25 @@
+﻿using Catalog.Application.Mappers;
+using Catalog.Application.Queries;
+using Catalog.Core.Application.Responses;
+using Catalog.Core.Repositories;
+using MediatR;
+
+namespace Catalog.Application.Handlers
+{
+    public class GetProductByIdHandler : IRequestHandler<GetProductByIdQuery, ProductResponse>
+    {
+        private readonly IProductRepository _productRepository;
+        public GetProductByIdHandler(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public async Task<ProductResponse> Handle(GetProductByIdQuery request, CancellationToken cancellationToken)
+        {
+            var product = await _productRepository.GetProductById(request.Id);
+            var productResponse = ProductMapper.mapper.Map<ProductResponse>(product);
+
+            return productResponse;
+        }
+    }
+}
