970c87b1fbf95a067f7cf55113b23633bddb1746
Annaati <Abdi17fatah@gmail.com>
2023-08-29 18:33:12 +0300

    Basket.Core: Create/Implemented BBaskte Core Project

---
+++ Services/Basket/Basket.Core/Enitities/ShoppingBasket.cs
@@ -0,0 +1,17 @@
+﻿namespace Basket.Core.Enitities
+{
+    public class ShoppingBasket
+    {
+        public string UserName { get; set; }
+
+        public ShoppingBasket()
+        {
+            
+        }
+
+        public ShoppingBasket(string userName)
+        {
+            UserName = userName;
+        }
+    }
+}
---
+++ Services/Basket/Basket.Core/Repositories/IBasketRepository.cs
@@ -0,0 +1,11 @@
+﻿using Basket.Core.Enitities;
+
+namespace Basket.Core.Repositories
+{
+    public interface IBasketRepository
+    {
+        Task<ShoppingBasket> GetBasket(string userName);
+        Task<ShoppingBasket> UpdateBasket(ShoppingBasket basket);
+        Task DeleteBasket(string userName);
+    }
+}
