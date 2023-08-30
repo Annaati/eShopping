52d23a1e81940b2c00b6ba2064b71cb569e94ef6
Annaati <Abdi17fatah@gmail.com>
2023-08-30 21:11:14 +0300

    Basket.Infrastructure: Basket Infrastructure Project Creation and Basic set up

---
+++ Services/Basket/Basket.Infrastructure/Repositories/ShoppingBasketRepository.cs
@@ -0,0 +1,23 @@
+﻿using Basket.Core.Entities;
+using Basket.Core.Repositories;
+
+namespace Basket.Infrastructure.Repositories
+{
+    public class ShoppingBasketRepository : IShoppingBasketRepository
+    {
+        public Task DeleteBasket(string userName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ShoppingBasket> GetBasket(string userName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ShoppingBasket> UpdateBasket(ShoppingBasket basket)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
