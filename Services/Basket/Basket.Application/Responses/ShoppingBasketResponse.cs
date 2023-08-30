b72c8a3fee891cf52ef7193bc1264572c4399e47
Annaati <Abdi17fatah@gmail.com>
2023-08-30 22:04:11 +0300

    Basket.Application: Implemented ShoppingBasketResponse and ShoppingBasketItemResponse Classes

--- Services/Basket/Basket.Application/Responses/ShoppingBasketResponse.cs
+++ Services/Basket/Basket.Application/Responses/ShoppingBasketResponse.cs
@@ -2,5 +2,33 @@
 {
     public class ShoppingBasketResponse
     {
+        public string UserName { get; set; }
+        public List<ShoppingBasketItemResponse> Items { get; set; }
+
+
+        public ShoppingBasketResponse()
+        {
+
+        }
+
+        public ShoppingBasketResponse(string userName)
+        {
+            UserName = userName;
+        }
+
+        public decimal TotalPrice
+        {
+            get
+            {
+                decimal totalPrice = 0;
+                foreach (var item in Items)
+                {
+                    totalPrice += item.Price * item.Quantity;
+                }
+                return totalPrice;
+            }
+        }
+
+
     }
 }
