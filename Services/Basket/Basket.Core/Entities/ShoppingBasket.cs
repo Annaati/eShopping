1eb3e45175450a353f4ec3b3645699b7767aa162
Annaati <Abdi17fatah@gmail.com>
2023-08-30 20:52:28 +0300

    Basket.Core: Basket cORE pROJECT Creation/Set up with Entities/Repositories Implementation

---
+++ Services/Basket/Basket.Core/Entities/ShoppingBasket.cs
@@ -0,0 +1,17 @@
+﻿namespace Basket.Core.Entities
+{
+    public class ShoppingBasket
+    {
+        public string UserName { get; set; }
+
+        public ShoppingBasket()
+        {
+
+        }
+
+        public ShoppingBasket(string userName)
+        {
+            UserName = userName;
+        }
+    }
+}
---
+++ Services/Basket/Basket.Core/Repositories/IShoppingBasketRepository.cs
@@ -0,0 +1,11 @@
+﻿using Basket.Core.Entities;
+
+namespace Basket.Core.Repositories
+{
+    public interface IShoppingBasketRepository
+    {
+        Task<ShoppingBasket> GetBasket(string userName);
+        Task<ShoppingBasket> UpdateBasket(ShoppingBasket basket);
+        Task DeleteBasket(string userName);
+    }
+}
