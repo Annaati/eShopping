50d9ad3e6499aa24cfd646a9b9e8fe4c7cba5af4
Annaati <Abdi17fatah@gmail.com>
2023-08-30 22:08:54 +0300

    Basket.Application: Implemented BasketMappingProfile and BasketMapper Classes

--- Services/Basket/Basket.Application/Mappers/BasketMappingProfile.cs
+++ Services/Basket/Basket.Application/Mappers/BasketMappingProfile.cs
@@ -1,6 +1,15 @@
-﻿namespace Basket.Application.Mappers
+﻿using AutoMapper;
+using Basket.Application.Responses;
+using Basket.Core.Entities;
+
+namespace Basket.Application.Mappers
 {
-    public class BasketMappingProfile
+    public class BasketMappingProfile : Profile
     {
+        public BasketMappingProfile()
+        {
+            CreateMap<ShoppingBasket, ShoppingBasketResponse>().ReverseMap();
+            CreateMap<ShoppingBasketItem, ShoppingBasketItemResponse>().ReverseMap();
+        }
     }
 }
