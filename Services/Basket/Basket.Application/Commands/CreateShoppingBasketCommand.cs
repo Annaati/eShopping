0cc76fab94195dafba7d31d2dca7f26c22d44f37
Annaati <Abdi17fatah@gmail.com>
2023-08-30 22:06:41 +0300

    Basket.Application: Implemented CreateShoppingBasketCommand Classe

--- Services/Basket/Basket.Application/Commands/CreateShoppingBasketCommand.cs
+++ Services/Basket/Basket.Application/Commands/CreateShoppingBasketCommand.cs
@@ -1,6 +1,17 @@
-﻿namespace Basket.Application.Commands
+﻿using Basket.Application.Responses;
+using Basket.Core.Entities;
+using MediatR;
+
+namespace Basket.Application.Commands
 {
-    public class CreateShoppingBasketCommand
+    public class CreateShoppingBasketCommand : IRequest<ShoppingBasketResponse>
     {
+        public CreateShoppingBasketCommand(string userName, List<ShoppingBasketItem> items)
+        {
+            UserName = userName;
+            Items = items;
+        }
+        public string UserName { get; set; }
+        public List<ShoppingBasketItem> Items { get; set; }
     }
 }
