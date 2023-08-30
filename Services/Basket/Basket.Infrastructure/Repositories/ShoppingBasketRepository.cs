c55aa7c260efd4ebde1691329e19f8b03211530d
Annaati <Abdi17fatah@gmail.com>
2023-08-30 21:26:03 +0300

    Basket.Infrastructure: ShoppingBasketRepository Implementation

--- Services/Basket/Basket.Infrastructure/Repositories/ShoppingBasketRepository.cs
+++ Services/Basket/Basket.Infrastructure/Repositories/ShoppingBasketRepository.cs
@@ -1,23 +1,46 @@
-﻿using Basket.Core.Entities;
+﻿using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
+using Basket.Core.Entities;
 using Basket.Core.Repositories;
 
 namespace Basket.Infrastructure.Repositories
 {
     public class ShoppingBasketRepository : IShoppingBasketRepository
     {
-        public Task DeleteBasket(string userName)
+        private readonly IDistributedCache _redisCache;
+
+        public ShoppingBasketRepository(IDistributedCache redisCache)
         {
-            throw new NotImplementedException();
+            _redisCache = redisCache;
         }
 
-        public Task<ShoppingBasket> GetBasket(string userName)
+
+
+        public async Task<ShoppingBasket> GetBasket(string userName)
         {
-            throw new NotImplementedException();
+            var basket = await _redisCache.GetStringAsync(userName);
+            if (string.IsNullOrEmpty(basket))
+                return null;
+
+            return JsonSerializer.Deserialize<ShoppingBasket>(basket);
         }
 
-        public Task<ShoppingBasket> UpdateBasket(ShoppingBasket basket)
+
+        public async Task<ShoppingBasket> UpdateBasket(ShoppingBasket basket)
         {
-            throw new NotImplementedException();
+            await _redisCache.SetStringAsync(basket.UserName, JsonSerializer.Serialize(basket));
+            return await GetBasket(basket.UserName);
         }
+
+
+        public async Task DeleteBasket(string userName)
+        {
+            await _redisCache.RemoveAsync(userName);
+        }
+
+
+
+
+
     }
 }
