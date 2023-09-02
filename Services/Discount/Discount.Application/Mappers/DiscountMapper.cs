696c6df858a69d26935a12a9fd55fdcb267c6bb7
Annaati <Abdi17fatah@gmail.com>
2023-09-02 23:59:13 +0300

    Discount.Application: Mappers Creation/Implementation

---
+++ Services/Discount/Discount.Application/Mappers/DiscountMapper.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+
+namespace Discount.Application.Mappers
+{
+    public class DiscountMapper
+    {
+        private static readonly Lazy<IMapper> Lazy = new Lazy<IMapper>(() =>
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
+                cfg.ShouldMapProperty = p => p.GetMethod.IsPublic || p.GetMethod.IsAssembly;
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
+                cfg.AddProfile<DiscountMappingProfile>();
+            });
+
+            var mapper = config.CreateMapper();
+            return mapper;
+        });
+
+        public static IMapper Mapper => Lazy.Value;
+    }
+}
