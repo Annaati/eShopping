bfd002dc0cc8768fbd3a603b2bb84a4fe48fd216
Annaati <Abdi17fatah@gmail.com>
2023-09-02 23:59:50 +0300

    Discount.Application: Handlers Creation/Implementation

---
+++ Services/Discount/Discount.Application/Handlers/DeleteDiscountHandler.cs
@@ -0,0 +1,21 @@
+﻿using Discount.Application.Commands;
+using Discount.Core.Repositories;
+using MediatR;
+
+namespace Discount.Application.Handlers
+{
+    internal class DeleteDiscountHandler : IRequestHandler<DeleteDiscountCommand, bool>
+    {
+        private readonly IDiscountRepository _discountRepository;
+
+        public DeleteDiscountHandler(IDiscountRepository discountRepository)
+        {
+            _discountRepository = discountRepository;
+        }
+        public async Task<bool> Handle(DeleteDiscountCommand request, CancellationToken cancellationToken)
+        {
+            var deleted = await _discountRepository.DeleteDiscount(request.ProductName);
+            return deleted;
+        }
+    }
+}
---
+++ Services/Discount/Discount.Application/Handlers/UpdateDiscountHandler.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using Discount.Application.Commands;
+using Discount.Application.Mappers;
+using Discount.Core.Entities;
+using Discount.Core.Repositories;
+using Discount.Grpc.Protos;
+using MediatR;
+
+namespace Discount.Application.Handlers
+{
+    public class UpdateDiscountHandler : IRequestHandler<UpdateDiscountCommand, CouponModel>
+    {
+        private readonly IDiscountRepository _discountRepository;
+
+        public UpdateDiscountHandler(IDiscountRepository discountRepository)
+        {
+            _discountRepository = discountRepository;
+        }
+        public async Task<CouponModel> Handle(UpdateDiscountCommand request, CancellationToken cancellationToken)
+        {
+            var coupon = _discountRepository.UpdateDiscount(new Coupon
+            {
+                Id = request.Id,
+                ProductName = request.ProductName,
+                Description = request.Description,
+                Amount = request.Amount
+            });
+
+            var couponModel = DiscountMapper.Mapper.Map<CouponModel>(coupon);
+
+            return couponModel;
+        }
+    }
+}
